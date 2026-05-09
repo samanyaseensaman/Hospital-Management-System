780ad7d8886b8c26b4a401ff91c6261aa01959b9
Wazzuha-Moazzam-1 <sp26-bse-091@cuilahore.edu.pk>
2026-05-09 23:53:14 +0500

    Update patients.cpp
    modified according to structs now, had to redo everything

--- patients.cpp
+++ patients.cpp
@@ -4,14 +4,6 @@
 #include <ctime>
 using namespace std;
 
-// hardcoded values and arrays defining
-// these values are only for testing the functions, may be removed or used later acc to group decision.
-int patientID;
-string patients[20]= {"alia","shazia","johnny","jugnu","basheer","ahmed talal","malik talal","shafia"};
-int patientAge [20] = {23,17,10,34,45,78,34,5};
-string diseases [20] = {"cholera","mad cow disease","malaria","cataract","anaemia","alzheimer's","diaorhhea","psychosis"};
-string bloodGrp[20] = {"O+","O-","A+","A+","B+","AB+","O+","A+"};
-int beds = 20;
 
 //functions
 
@@ -20,29 +12,45 @@ int beds = 20;
 // void admitPatient();
 // void availableBeds();
 // void BillOfPatient();
+//void displayAllPatients();
 
-//starting with the functions now
-// the function isnt complete yet, have to make it purely file reader isntead of looping through arrays within the code.
-void patientsInfo(){
-    cout<<"enter the patient ID whose info you would like to see.\n";
-    cin>>patientID;
-    for (int p = 0; p<=20;p++){
-        if (p == patientID-1){
-        cout<< "patients name:"<<patients[p]<<endl;
-        cout<< "patients age:"<<patientAge[p]<<endl;
-        cout<< "patients disease:"<<diseases[p]<<endl;
-        cout<<"patients blood group is: "<<bloodGrp[p]<<endl;
-        cout<<"patients bed number: "<<patientID<<endl;
-        }
-        
-        
+// changes made because structs are allowed now  ~~~~~~~~~~~~~~~~~~
+
+struct Patient {
+    int patientID;
+    string name;
+    int age;
+    string disease;
+    string bloodGroup;
+    string admissionDate;
+    double totalBill;
+};
+
+// Struct to manage the hospital operations
+struct PatientManager {
+    int TOTAL_HOSPITAL_BEDS = 20;
+    Patient patients[20]; 
+    int patientCount = 0; 
+    
+    double RATE_PER_DAY = 500.0;
+    string FILENAME = "patientsinfo.csv";
+
+ void displayAllPatients() {
+    if (patientCount == 0) {
+        cout << "\nNo patients currently admitted.\n";
+        return;
     }
 
-
+    for (int i = 0; i < patientCount; i++) {
+        cout <<"patients ID: "<< patients[i].patientID << "\n"
+             <<"name: "<<patients[i].name<< "\n" 
+             <<"age: "<< patients[i].age << "\n"
+             <<"disease: "<< patients[i].disease << "\n"
+             <<"blood group: "<< patients[i].bloodGroup << "\n"
+             <<"date of admission: "<< patients[i].admissionDate << "\n"
+             <<"total bill for stay: "<< "$" << patients[i].totalBill << "\n";
+    }
+    }
 
 }
-
-int main(){
-
-    patientsInfo();
-}
+    
