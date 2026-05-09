0506649e022e79e21c88ab4d4b192b60becc54c9
Wazzuha-Moazzam-1 <sp26-bse-091@cuilahore.edu.pk>
2026-05-09 19:48:05 +0500

    Update comments on  patients.cpp for clarification of status

--- patients.cpp
+++ patients.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 // hardcoded values and arrays defining
+// these values are only for testing the functions, may be removed or used later acc to group decision.
 int patientID;
 string patients[20]= {"alia","shazia","johnny","jugnu","basheer","ahmed talal","malik talal","shafia"};
 int patientAge [20] = {23,17,10,34,45,78,34,5};
@@ -21,6 +22,7 @@ int beds = 20;
 // void BillOfPatient();
 
 //starting with the functions now
+// the function isnt complete yet, have to make it purely file reader isntead of looping through arrays within the code.
 void patientsInfo(){
     cout<<"enter the patient ID whose info you would like to see.\n";
     cin>>patientID;
