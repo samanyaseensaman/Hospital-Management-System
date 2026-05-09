3f3f7a7b25d5f0c6662585a079d9465f37c76645
shaihram <shaihramnoor12@gmail.com>
2026-05-09 21:17:08 +0500

    Updated doctor.cpp

--- doctor.cpp
+++ doctor.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-/*       Structures For Doctor And Patients      */
+/*       Structures For Doctor           */
 
 struct Doctor
 {
@@ -17,15 +17,6 @@ struct Doctor
     string experience;
 };
 
-struct Patient {
-    int patientID;
-    string name;
-    int doctorID;
-    string disease;
-    string bloodGroup;
-    double bill;
-};
-
 /*       For DMA     */
 
 Doctor* doctors = nullptr;
@@ -36,11 +27,14 @@ int count = 0;
 /*       Functions     */
 
 void loadDoctors();
+void saveDoctors();
+
+void doctorMenu();
+
 void addDoctor();
 void viewDoctors();
 void searchDoctor();
-void doctorPatients();
-void doctorMenu();
+void updateDoctor();
 
 
 
@@ -77,53 +71,41 @@ int main() {
                             /*    Definition of Functions    */
 
 
-
+/*        Loading Doctors       */
 
 
 void loadDoctors()
 {
-
-        delete[] doctors;
-        doctors = nullptr;
-        count = 0;
+    delete[] doctors;
+    doctors = nullptr;
+    count = 0;
 
     ifstream file("doctors.csv");
-
-    if(!file)
-    {
-      return;
-    }
+    if(!file) return;
 
     string line;
-
     while(getline(file, line))
-    {
         count++;
-    }
 
-    file.close();
-
-    if(count == 0)
-    {
-        doctors = nullptr;
-        return;
-    }
+    if(count == 0) return;
 
     doctors = new Doctor[count];
 
-    file.open("doctors.csv");
+    file.clear();
+    file.seekg(0);
 
     for(int i = 0; i < count; i++)
     {
-        file >> doctors[i].id;
-        file.ignore();
+        string idStr;
+
+        getline(file, idStr, ',');
+        doctors[i].id = stoi(idStr);
 
         getline(file, doctors[i].name, ',');
         getline(file, doctors[i].specialization, ',');
         getline(file, doctors[i].department, ',');
         getline(file, doctors[i].timing, ',');
         getline(file, doctors[i].availability, ',');
-
         getline(file, doctors[i].experience);
     }
 
@@ -131,6 +113,26 @@ void loadDoctors()
 }
 
 
+
+/*          Save Doctors         */
+
+void saveDoctors()
+{
+    ofstream file("doctors.csv");
+
+    for(int i = 0; i < count; i++)
+    {
+        file << doctors[i].id << ","
+             << doctors[i].name << ","
+             << doctors[i].specialization << ","
+             << doctors[i].department << ","
+             << doctors[i].timing << ","
+             << doctors[i].availability << ","
+             << doctors[i].experience << "\n";
+    }
+
+    file.close();
+}
 
 
 
@@ -146,7 +148,7 @@ do  {
         cout << "\n1. Add Doctor";
         cout << "\n2. View Doctors";
         cout << "\n3. Search Doctor";
-        cout << "\n4. View Doctor Patients";
+        cout << "\n4. Update Information";
         cout << "\n0. Back";
         cout << "\nEnter choice: ";
         cin >> choice;
@@ -159,8 +161,9 @@ do  {
             case 1: addDoctor(); break;
             case 2: viewDoctors(); break;
             case 3: searchDoctor(); break;
-            case 4: doctorPatients(); break;
-
+            case 4: updateDoctor(); break;
+            default:
+            cout << "Invalid choice!\n";
         }
 }while(choice !=0);
 }
@@ -180,6 +183,16 @@ cout << "Enter Doctor ID: ";
     cin >> newDoc.id;
 
     cin.ignore();
+
+
+      for(int i = 0; i < count; i++)
+    {
+        if(doctors[i].id == newDoc.id)
+        {
+            cout << "Doctor ID already exists!\n";
+            return;
+        }
+    }
 
     cout << "Enter Name: ";
     getline(cin, newDoc.name);
@@ -201,39 +214,21 @@ cout << "Enter Doctor ID: ";
 
 
 
-  Doctor* temp = new Doctor[count + 1]; // dynamic array
+    Doctor* temp = new Doctor[count + 1];
 
-    for(int i = 0; i < count; i++) //copy old data into new
-    {
+    for(int i = 0; i < count; i++)
         temp[i] = doctors[i];
-    }
 
-    temp[count] = newDoc; //adds new doc
+    temp[count] = newDoc;
 
     delete[] doctors;
-
     doctors = temp;
-
     count++;
 
-    ofstream file("doctors.csv", ios::app);
-
-    if(file.is_open())
-    {
-        file << newDoc.id << ","
-             << newDoc.name << ","
-             << newDoc.specialization << ","
-             << newDoc.department << ","
-             << newDoc.timing << ","
-             << newDoc.availability << ","
-             << newDoc.experience << endl;
-
-        file.close();
-    }
+    saveDoctors();
 
     cout << "\nDoctor Added Successfully!\n";
 }
-
 
 /*         View Doctors           */
 
@@ -265,7 +260,132 @@ void viewDoctors()
 }
 
 
+/*          Search Doctor          */
+
+void searchDoctor()
+{
+    int searchID;
+    bool found = false;
+
+    cout << "\nEnter Doctor ID: ";
+    cin >> searchID;
+
+    for(int i = 0; i < count; i++)
+    {
+        if(doctors[i].id == searchID)
+        {
+            found = true;
+
+            cout << "\nDoctor Found!\n";
+            cout << "\nID: " << doctors[i].id;
+            cout << "\nName: " << doctors[i].name;
+            cout << "\nSpecialization: " << doctors[i].specialization;
+            cout << "\nDepartment: " << doctors[i].department;
+            cout << "\nTiming: " << doctors[i].timing;
+            cout << "\nAvailability: " << doctors[i].availability;
+            cout << "\nExperience: " << doctors[i].experience;
+            break;
+        }
+    }
+
+    if(!found)
+        cout << "\nDoctor Not Found!\n";
+}
 
 
+
+/*          For Corrections            */
+void updateDoctor()
+{
+    int id;
+    bool found = false;
+    char choice;
+
+    cout << "Enter Doctor ID to update: ";
+    cin >> id;
+    cin.ignore();
+
+    for(int i = 0; i < count; i++)
+    {
+        if(doctors[i].id == id)
+        {
+            found = true;
+
+            cout << "\n--- CURRENT DATA ---\n";
+            cout << "Name: " << doctors[i].name << endl;
+            cout << "Specialization: " << doctors[i].specialization << endl;
+            cout << "Department: " << doctors[i].department << endl;
+            cout << "Timing: " << doctors[i].timing << endl;
+            cout << "Availability: " << doctors[i].availability << endl;
+            cout << "Experience: " << doctors[i].experience << endl;
+
+            cout << "\n--- UPDATE OPTIONS ---\n";
+
+            cout << "Change Name? (y/n): ";
+            cin >> choice;
+            cin.ignore();
+            if(choice == 'y' || choice == 'Y')
+            {
+                cout << "Enter new name: ";
+                getline(cin, doctors[i].name);
+            }
+
+            cout << "Change Specialization? (y/n): ";
+            cin >> choice;
+            cin.ignore();
+            if(choice == 'y' || choice == 'Y')
+            {
+                cout << "Enter new specialization: ";
+                getline(cin, doctors[i].specialization);
+            }
+
+            cout << "Change Department? (y/n): ";
+            cin >> choice;
+            cin.ignore();
+            if(choice == 'y' || choice == 'Y')
+            {
+                cout << "Enter new department: ";
+                getline(cin, doctors[i].department);
+            }
+
+            cout << "Change Timing? (y/n): ";
+            cin >> choice;
+            cin.ignore();
+            if(choice == 'y' || choice == 'Y')
+            {
+                cout << "Enter new timing: ";
+                getline(cin, doctors[i].timing);
+            }
+
+            cout << "Change Availability? (y/n): ";
+            cin >> choice;
+            cin.ignore();
+            if(choice == 'y' || choice == 'Y')
+            {
+                cout << "Enter new availability: ";
+                getline(cin, doctors[i].availability);
+            }
+
+            cout << "Change Experience? (y/n): ";
+            cin >> choice;
+            cin.ignore();
+            if(choice == 'y' || choice == 'Y')
+            {
+                cout << "Enter new experience: ";
+                getline(cin, doctors[i].experience);
+            }
+
+            saveDoctors();
+
+            cout << "\nDoctor Updated Successfully!\n";
+            break;
+        }
+    }
+
+    if(!found)
+    {
+        cout << "\nDoctor Not Found!\n";
+    }
+}
 
 
